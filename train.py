0c638a851202eceb99a0b7fea5bd0a74bb8ebd0e
Alex <mboutchouangalex+github@gmail.com>
2023-02-22 13:26:44 +0100

    Trainer

--- train.py
+++ train.py
@@ -2,3 +2,28 @@ import torch
 import pytorch_lightning as pl
 from model import SentimentAnalysis
 from datamodule import TweetDataModule
+from pathlib import Path
+
+DATA_PATH = Path("./clean_data.csv")
+BERT_MODEL = "bert-base-cased"
+LEARNING_RATE = 1e-3
+MAX_LENGTH = 240  # max length of a tweet
+NUM_EPOCHS = 10
+
+
+cls = SentimentAnalysis(
+    bert_model=BERT_MODEL,
+    num_classes=3,
+    lr=LEARNING_RATE
+)
+
+data = TweetDataModule(
+    data_path=DATA_PATH,
+    bert_model=BERT_MODEL,
+    max_length=MAX_LENGTH,
+    batch_size=1
+)
+
+trainer = pl.Trainer(max_epochs=NUM_EPOCHS)
+trainer.fit(cls, data)
+trainer.save_checkpoint("trained_model", weights_only=True)
