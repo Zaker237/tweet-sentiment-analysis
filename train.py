7ebac2d0cb74a4c5f56d9367e9cd55218df734f0
Alex <mboutchouangalex+github@gmail.com>
2023-02-23 23:43:18 +0100

    fix trainer

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ DATA_PATH = Path("./clean_data.csv")
 BERT_MODEL = "bert-base-cased"
 LEARNING_RATE = 1e-3
 MAX_LENGTH = 240  # max length of a tweet
-NUM_EPOCHS = 10
+NUM_EPOCHS = 3
 
 
 cls = SentimentAnalysis(
@@ -21,9 +21,11 @@ data = TweetDataModule(
     data_path=DATA_PATH,
     bert_model=BERT_MODEL,
     max_length=MAX_LENGTH,
-    batch_size=16
+    batch_size=4
 )
 
 trainer = pl.Trainer(max_epochs=NUM_EPOCHS)
 trainer.fit(cls, data)
+cls.eval()
+trainer.test(data)
 trainer.save_checkpoint("trained_model", weights_only=True)
